c2196410875249cc0ef8c85760d7785a1f40725d
Christopher Stawarz <cstawarz@mit.edu>
2010-12-17 15:30:42 -0500

    Explicitly save/set/restore all OpenGL pixel unpack parameters

--- WhiteNoiseBackground.cpp
+++ WhiteNoiseBackground.cpp
@@ -41,9 +41,23 @@ void WhiteNoiseBackground::load(shared_ptr<StimulusDisplay> display) {
 
 
 void WhiteNoiseBackground::draw(shared_ptr<StimulusDisplay> display) {
+    glPushClientAttrib(GL_CLIENT_PIXEL_STORE_BIT);
+
+    // Set all pixel unpack parameters to default values
+    glPixelStorei(GL_UNPACK_SWAP_BYTES, GL_FALSE);
+    glPixelStorei(GL_UNPACK_LSB_FIRST, GL_FALSE);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+    glPixelStorei(GL_UNPACK_IMAGE_HEIGHT, 0);
+    glPixelStorei(GL_UNPACK_SKIP_ROWS, 0);
+    glPixelStorei(GL_UNPACK_SKIP_PIXELS, 0);
+    glPixelStorei(GL_UNPACK_SKIP_IMAGES, 0);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+
     DisplayDimensions &currentDims = dims[display->getCurrentContextIndex()];
     glWindowPos2i(0, 0);
     glDrawPixels(currentDims.first, currentDims.second, GL_LUMINANCE, PIXEL_TYPE, &(pixels[0]));
+
+    glPopClientAttrib();
 }
 
 
