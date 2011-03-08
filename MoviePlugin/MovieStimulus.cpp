ff2ab6d3d2da0d23d40e352253d6f0c96d9a411a
Christopher Stawarz <cstawarz@mit.edu>
2011-03-08 12:38:09 -0500

    Added factory for ImageDirectoryMovieStimulus

--- MoviePlugin/MovieStimulus.cpp
+++ MoviePlugin/MovieStimulus.cpp
@@ -153,47 +153,131 @@ Datum ImageDirectoryMovieStimulus::getCurrentAnnounceDrawData() {
 }
 
 
-shared_ptr<mw::Component> MovieStimulusFactory::createObject(std::map<std::string, std::string> parameters,
-                                                             ComponentRegistry *reg)
+void BaseMovieStimulusFactory::getBaseMovieParameters(std::map<std::string, std::string> &parameters,
+                                                      ComponentRegistry *reg,
+                                                      std::string &tag,
+                                                      shared_ptr<Variable> &framesPerSecond,
+                                                      shared_ptr<Variable> &ended,
+                                                      shared_ptr<Variable> &loop)
 {
     const char *TAG = "tag";
-    const char *STIMULUS_GROUP = "stimulus_group";
     const char *FRAMES_PER_SECOND = "frames_per_second";
     const char *ENDED = "ended";
     const char *LOOP = "loop";
     
-    REQUIRE_ATTRIBUTES(parameters, 
-                       TAG, 
-                       STIMULUS_GROUP,
+    REQUIRE_ATTRIBUTES(parameters,
+                       TAG,
                        FRAMES_PER_SECOND);
     
-    std::string tagname(parameters[TAG]);
+    tag = parameters[TAG];
     
-    shared_ptr<StimulusGroup> stimulusGroup(reg->getObject<StimulusGroup>(parameters[STIMULUS_GROUP]));
-    CHECK_ATTRIBUTE(stimulusGroup, parameters, STIMULUS_GROUP);
-    
-    shared_ptr<Variable> framesPerSecond(reg->getVariable(parameters[FRAMES_PER_SECOND]));    
+    framesPerSecond = reg->getVariable(parameters[FRAMES_PER_SECOND]);
     CHECK_ATTRIBUTE(framesPerSecond, parameters, FRAMES_PER_SECOND);
-
-    shared_ptr<Variable> ended;
+    
     if (!(parameters[ENDED].empty())) {
         ended = reg->getVariable(parameters[ENDED]);
         CHECK_ATTRIBUTE(ended, parameters, ENDED);
     }
-
-    shared_ptr<Variable> loop(reg->getVariable(parameters[LOOP], "0"));
+    
+    loop = reg->getVariable(parameters[LOOP], "0");
     CHECK_ATTRIBUTE(loop, parameters, LOOP);
+}
+
+
+shared_ptr<mw::Component> MovieStimulusFactory::createObject(std::map<std::string, std::string> parameters,
+                                                             ComponentRegistry *reg)
+{
+    const char *STIMULUS_GROUP = "stimulus_group";
     
-    shared_ptr <MovieStimulus> new_movie = shared_ptr<MovieStimulus>(new MovieStimulus(tagname,
-                                                                                       framesPerSecond,
-                                                                                       stimulusGroup,
-                                                                                       ended,
-                                                                                       loop));
+    REQUIRE_ATTRIBUTES(parameters, STIMULUS_GROUP);
     
-    shared_ptr <StimulusNode> thisStimNode(new StimulusNode(new_movie));
-    reg->registerStimulusNode(tagname, thisStimNode);
+    std::string tag;
+    shared_ptr<Variable> framesPerSecond;
+    shared_ptr<Variable> ended;
+    shared_ptr<Variable> loop;
+    getBaseMovieParameters(parameters, reg, tag, framesPerSecond, ended, loop);
     
-    return new_movie;
+    shared_ptr<StimulusGroup> stimulusGroup(reg->getObject<StimulusGroup>(parameters[STIMULUS_GROUP]));
+    CHECK_ATTRIBUTE(stimulusGroup, parameters, STIMULUS_GROUP);
+    
+    shared_ptr<MovieStimulus> newMovie(new MovieStimulus(tag,
+                                                         framesPerSecond,
+                                                         stimulusGroup,
+                                                         ended,
+                                                         loop));
+    
+    // To preserve existing behavior, *do not* load stimulus here
+    shared_ptr<StimulusNode> node(new StimulusNode(newMovie));
+    reg->registerStimulusNode(tag, node);
+    
+    return newMovie;
+}
+
+
+shared_ptr<mw::Component>
+ImageDirectoryMovieStimulusFactory::createObject(std::map<std::string, std::string> parameters, ComponentRegistry *reg)
+{
+    const char *PATH = "path";
+    const char *X_SIZE = "x_size";
+    const char *Y_SIZE = "y_size";
+    const char *X_POSITION = "x_position";
+    const char *Y_POSITION = "y_position";
+    const char *ROTATION = "rotation";
+    const char *ALPHA_MULTIPLIER = "alpha_multiplier";
+    const char *DEFERRED = "deferred";
+    
+    REQUIRE_ATTRIBUTES(parameters,
+                       PATH,
+                       X_SIZE,
+                       Y_SIZE);
+    
+    std::string tag;
+    shared_ptr<Variable> framesPerSecond;
+    shared_ptr<Variable> ended;
+    shared_ptr<Variable> loop;
+    getBaseMovieParameters(parameters, reg, tag, framesPerSecond, ended, loop);
+    
+    boost::filesystem::path directoryPath(reg->getPath(parameters["working_path"], parameters[PATH]));
+    
+    shared_ptr<Variable> xSize(reg->getVariable(parameters[X_SIZE]));
+    CHECK_ATTRIBUTE(xSize, parameters, X_SIZE);
+    
+    shared_ptr<Variable> ySize(reg->getVariable(parameters[Y_SIZE]));
+    CHECK_ATTRIBUTE(ySize, parameters, Y_SIZE);
+    
+    shared_ptr<Variable> xPosition(reg->getVariable(parameters[X_POSITION], "0.0"));
+    CHECK_ATTRIBUTE(xPosition, parameters, X_POSITION);
+    
+    shared_ptr<Variable> yPosition(reg->getVariable(parameters[Y_POSITION], "0.0"));
+    CHECK_ATTRIBUTE(yPosition, parameters, Y_POSITION);
+    
+    shared_ptr<Variable> rotation(reg->getVariable(parameters[ROTATION], "0.0"));
+    CHECK_ATTRIBUTE(rotation, parameters, ROTATION);
+    
+    shared_ptr<Variable> alphaMultiplier(reg->getVariable(parameters[ALPHA_MULTIPLIER], "1.0"));
+    CHECK_ATTRIBUTE(alphaMultiplier, parameters, ALPHA_MULTIPLIER);
+    
+    shared_ptr<ImageDirectoryMovieStimulus> newMovie(new ImageDirectoryMovieStimulus(tag,
+                                                                                     directoryPath.string(),
+                                                                                     xSize,
+                                                                                     ySize,
+                                                                                     xPosition,
+                                                                                     yPosition,
+                                                                                     rotation,
+                                                                                     alphaMultiplier,
+                                                                                     framesPerSecond,
+                                                                                     ended,
+                                                                                     loop));
+    
+    newMovie->setDeferredFromString(parameters[DEFERRED]);
+    if (newMovie->getDeferred() == Stimulus::nondeferred_load) {
+        newMovie->load(StimulusDisplay::getCurrentStimulusDisplay());
+    }
+
+    shared_ptr<StimulusNode> node(new StimulusNode(newMovie));
+    reg->registerStimulusNode(tag, node);
+    
+    return newMovie;
 }
 
 
--- MoviePlugin/MovieStimulus.h
+++ MoviePlugin/MovieStimulus.h
@@ -113,7 +113,26 @@ private:
 };
 
 
-class MovieStimulusFactory : public ComponentFactory {
+class BaseMovieStimulusFactory : public ComponentFactory {
+
+protected:
+    void getBaseMovieParameters(std::map<std::string, std::string> &parameters,
+                                ComponentRegistry *reg,
+                                std::string &tag,
+                                shared_ptr<Variable> &framesPerSecond,
+                                shared_ptr<Variable> &ended,
+                                shared_ptr<Variable> &loop);
+
+};
+
+
+class MovieStimulusFactory : public BaseMovieStimulusFactory {
+    virtual shared_ptr<mw::Component> createObject(std::map<std::string, std::string> parameters,
+                                                   ComponentRegistry *reg);
+};
+
+
+class ImageDirectoryMovieStimulusFactory : public BaseMovieStimulusFactory {
     virtual shared_ptr<mw::Component> createObject(std::map<std::string, std::string> parameters,
                                                    ComponentRegistry *reg);
 };
